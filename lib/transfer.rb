d1e3ce38e9e839581089af7172098db142095c57
Steven Topazio <topazio.steven@gmail.com>
2020-10-27 19:51:13 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,7 @@ class Transfer
   end
   
   def execute_transaction
-    if valid? = false or @sender.balance < @amount
+    if @sender.balance < @amount
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
 
