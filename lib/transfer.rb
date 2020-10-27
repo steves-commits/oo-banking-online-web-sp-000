0c20f5df569089ca7aea552e59be0eb33311ec0c
Steven Topazio <topazio.steven@gmail.com>
2020-10-27 20:08:51 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,7 +2,7 @@ class Transfer
   
   attr_accessor :sender, :receiver, :status, :amount 
   
-  def initialize(sender, receiver, amount, status='pending')
+  def initialize(sender, receiver, amount)
     @sender = sender 
     @receiver = receiver 
     @status = 'pending'
