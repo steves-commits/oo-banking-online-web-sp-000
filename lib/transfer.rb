e69f54a874e1ab5bc99639cd33d97d9b867b8cdd
Steven Topazio <topazio.steven@gmail.com>
2020-10-27 19:41:41 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -31,7 +31,12 @@ class Transfer
   end
 
 def reverse_transfer
-  
+  if @status == "complete"
+    @sender.deposit( @amount ) 
+    @receiver.deposit( @amount * -1)
+    @status = "reversed"
+  end
+end
 
   
   
